9ddcb828947a435c15f6cbd8ddafe37fd3823bbc
lam-letung <lam171ttt@gmail.com>
2024-09-29 01:35:10 +0700

    API_SERVER

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack'); // Chỉnh sửa cách import
 
 module.exports = {
-  entry: './src/public/client.js',        // Điểm vào cho Webpack (client.js)
+  entry: './src/public/client.js', // Điểm vào cho Webpack (client.js)
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'client.bundle.js'          // File JS sau khi bundle
+    path: path.resolve(__dirname, 'dist'), // Thư mục đầu ra
+    filename: 'client.bundle.js' // File JS sau khi bundle
   },
   
   module: {
     rules: [
       {
-        test: /\.css$/,                   // Load các file CSS
+        test: /\.css$/, // Load các file CSS
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.js$/,                    // Load các file JS
+        test: /\.js$/, // Load các file JS
         exclude: /node_modules/,
         use: 'babel-loader'
       }
@@ -25,17 +26,17 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/public/index.html', // File HTML gốc
-      filename: 'index.html'               // Copy vào thư mục dist
+      filename: 'index.html' // Copy vào thư mục dist
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'src/public/assets', to: 'assets' }  // Copy thư mục assets vào dist
+        { from: 'src/public/assets', to: 'assets' } // Copy thư mục assets vào dist
       ]
     }),
     new webpack.DefinePlugin({
-        'process.env.API_DOMAIN': JSON.stringify(process.env.API_DOMAIN),
-        'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-      })
+      'process.env.API_DOMAIN': JSON.stringify(process.env.API_DOMAIN), // Định nghĩa biến môi trường
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    })
   ],
-  mode: 'production'
+  mode: 'production' // Chế độ build
 };
